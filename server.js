c65b1bb6b36cf3d712f6efa40b1a006cfbb5f2c2
Angel Candas <angelcs790@gmail.com>
2018-09-03 15:41:35 +0200

    subida a heroku 0.07

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(cors()) //IMPORTANTE VERIFICACION CROSS ORIGINS
 app.use(bodyParser.json()); // CONTROLADOR DE PARSEO DE JSON
 
 app.get('/',(req,res)=>{res.status(200).json("Everything works fine!")}) // RESPUESTA POR DEFECTO DEL SERVIDOR
-
+app.get('/mqtt',(req,res)=>{console.log(req)});
 app.get('/profile/:id',(req,res)=>{profile.profileHandler(req,res,db)})
 
 app.post('/token',(req,res)=>{dataget.tokenGen(req,res,db)})
